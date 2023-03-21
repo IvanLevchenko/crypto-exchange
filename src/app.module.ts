0aad799d6023ab1e80ef1818f7add5017d867c09
Ivan Levchenko <ivan.levchenko@plus4u.net>
2023-03-21 16:33:45 +0200

    fix: removed redundant app controller/service, added missing packages for db; feat: added postgres connection with typeorm to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,24 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
 import { ConfigModule } from "@nestjs/config";
 
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
+import { User } from "./user/user.entity";
 
 @Module({
-  imports: [ConfigModule.forRoot()],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE,
+      entities: [User],
+      synchronize: true,
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
